23e7b5111b344942a539ec6e0b6ef5b9ffee9ea9
Mniok <58875711+Mniok@users.noreply.github.com>
2022-08-21 14:01:40 +0200

    django project w. heroku database
    Django project prepared
    Heroku postgres database connected

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aplikacja-pogody
+przed uruchomieniem programu należy zainstalować kilka rzeczy z wiersza poleceń (upewnij się, że znajdujesz się w folderze ./aplikacja-pogody, w którym znajduje się manage.py):
+1.pip install django
+2.pip install install crispy-bootstrap5
+3.pip install psycopg2
+
+program uruchamia się komendą python manage.py runserver
---
+++ authentication/forms.py
@@ -0,0 +1,13 @@
+from socket import fromshare
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    #email = forms.EmailField(required=True, label='podaj email swój testtest') #to dziala
+    #email.label też działa
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
