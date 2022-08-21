83ec5caa9ab67fc747721f2fde0fae32c6c6c444
Mniok <58875711+Mniok@users.noreply.github.com>
2022-08-21 22:22:35 +0200

    heroku troubleshooting
    staticfiles raised an exception, disabled for now

--- Pogoda/settings.py
+++ Pogoda/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+#    'django.contrib.staticfiles',
     'authentication.apps.AuthenticationConfig',
     'crispy_forms',
     'crispy_bootstrap5',
@@ -126,7 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+#STATIC_ROOT = "/var/www/pogoda.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
