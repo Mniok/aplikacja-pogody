e22857f44c5a87fb230ed74934e159ff8a3ddbf2
Mniok <58875711+Mniok@users.noreply.github.com>
2022-08-30 14:25:54 +0200

    static root change
    static root changed to vars/www because var/www is read-only on Heroku dyno

--- Pogoda/settings.py
+++ Pogoda/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'authentication.apps.AuthenticationConfig',
     'crispy_forms',
     'crispy_bootstrap5',
+    'injector',
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -130,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = "/var/www/pogoda.com/static/"
+STATIC_ROOT = "/vars/www/pogoda.com/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
